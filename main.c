dc763e252be89eec39cab491c9af8fd6524f07e5
MigueGsC <56319364+MigueGsC@users.noreply.github.com>
2020-01-03 19:40:04 -0300

    Version del main
    aca va lo que hice

--- main.c
+++ main.c
@@ -29,6 +29,86 @@ int stringEqual(const void * key1, const void * key2) {
 
 int main()
 {
-    printf("Hewwo");
+    int op;
+
+    printf("Hewwo\n");
     return 0;
+
+    while(op) //Menu
+    {
+      printf("1.-A\n");
+      printf("2.-B\n");
+      printf("3.-C\n");
+      printf("4.-D\n");
+      printf("5.-E\n");
+      printf("6.-F\n");
+      printf("7.-G\n");
+      printf("8.-H\n");
+      printf("9.-Salir\n");
+      printf("Escoja una opcion -> ");
+
+      scanf("%i",&op);
+
+      if( op < 1 || op > 9)
+      {
+        system("cls");  //Limpia pantalla
+        printf("Ingrese una opcion valida\n");
+        getchar();
+        getchar();
+        system("cls");
+      }
+
+      switch (op)
+      {
+          case 1:
+
+                system("cls");
+                break;
+
+
+          case 2:
+
+                system("cls");
+                break;
+
+          case 3:
+
+                system("cls");
+                break;
+
+
+          case 4:
+
+                system("cls");
+                break;
+
+          case 5:
+
+                system("cls");
+                break;
+
+          case 6:
+
+                system("cls");
+                break;
+
+          case 7:
+
+                system("cls");
+                break;
+
+          case 8:
+
+                system("cls");
+                break;
+
+
+          case 9: //salir
+                system("cls");
+                exit (1);
+                break;
+
+        }
+
+    }
 }
